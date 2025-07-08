3891daf4086bb3925182c85cbc691de19e76119a
IndigoZuma <enzopublicity@proton.me>
2025-07-08 19:31:34 -0400

    TwoWayLinkedList.java

---
+++ TwoWayLinkedList.java
@@ -0,0 +1,7 @@
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class TwoWayLinkedList<E> implements MyList<E> {
+    // ... (implementation as shown previously)
+    // Use the implementation you already have, referencing public Node<E>
+}
